bc655a8bfbdd28a14048eefc747a6020b9831aca
Seo Hyeon Myeong <37466204+SeoHyeonMyeong@users.noreply.github.com>
2018-09-14 20:18:05 +0900

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+// 기본적으로 HTML에 사용된다.
+// <script>  </script> 태그 사이에 코드를 적는것이 기본이다.
+
+
+// 1. 이벤트
+// 
+// <input type="button" value="hi" onclick="alert('hi')">   버튼이 클릭되었을시 이벤트
+// <input type="text" onchange="alert('changed')">          텍스트가 바뀌었을때 이벤트
+// <input type="text" onkeydown="alert('key down!')">       키가 눌렸을때의 이벤트
+
+// 2. 콘솔
+// Ctrl + Shift + I : 구글 개발자도구 창 열기
+// Console 클릭 or ESC : 콘솔창 열기
+// 콘솔창에서 자바 스크립트를 입력하면 마치 웹에 자바스크립트를 쓴것과 같은 효과 발생
+// ex) alert('asdfasdfasdf'.length);
+
+// 3. 
